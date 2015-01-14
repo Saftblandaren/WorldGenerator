3c9b1d7e6a1c5c4be7a8c3892651af774b10dd3e
Saftblandaren <samuel.j.matti@gmail.com>
2015-01-14 15:24:07 +0100

    Update Capital.java

--- src/worldGenerator/Capital.java
+++ src/worldGenerator/Capital.java
@@ -1,11 +1,16 @@
 package worldGenerator;
 
-import java.util.Random;
-
 public class Capital extends Camp {
 
 	public Capital(World world, int slotX, int slotY) {
 		super(world, slotX, slotY);
+		
+		// Make Radius larger for capital
+		sizeRadius += (int) (world.getSLOT_SIZE()*0.1f + random.nextInt((int) (world.getSLOT_SIZE()*0.1f)));
+		
+		// And following has to be remade
+		setPosition();
+		setOutline();
 	}
 
 }
