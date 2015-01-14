5d926f5892fd33429df9fde2610d71f415ca4bd4
Saftblandaren <samuel.j.matti@gmail.com>
2015-01-14 15:24:33 +0100

    Update ImageGenerator.java

--- src/worldGenerator/ImageGenerator.java
+++ src/worldGenerator/ImageGenerator.java
@@ -1,11 +1,8 @@
 package worldGenerator;
 
-import helpers.Spline2D;
-
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
 
 import javax.imageio.ImageIO;
 
@@ -13,32 +10,36 @@ public class ImageGenerator {
 
 	public ImageGenerator(World world) {
 		
-		int size = world.getSize();
-		BufferedImage img = new BufferedImage(size, size, BufferedImage.TYPE_INT_ARGB);
-		
-		File f = null;
-		
-		int a = 255;
-		int r=0;
-		int g=0;
-		int b=0;
-		
-		int p = (a<<24) | (r<<16) | (g<<8) | b;
-		
-		for(int i = 1; i < size-1; i++){
-			for(int j= 1;j < size-1; j++){
-				if(world.getValue(i, j) == 1)
-					img.setRGB(i, j, p);
+		int size = world.getSLOT_SIZE();
+		for(int slotX = 0; slotX < world.getSlots(); slotX++){
+			for(int slotY = 0; slotY < world.getSlots(); slotY++){
+				BufferedImage img = new BufferedImage(size, size, BufferedImage.TYPE_INT_ARGB);
+				File f = null;
+				
+				int a = 255;
+				int r=0;
+				int g=0;
+				int b=0;
+				
+				int p = (a<<24) | (r<<16) | (g<<8) | b;
+				
+				for(int x = 0; x < size; x++){
+					for(int y = 0; y < size; y++){
+						if(world.getValue(slotX*x, slotY*y) == 1){
+							img.setRGB(x, y, p);
+						}
+					}
+				}
+				
+				try{
+					f= new File("world_" + slotX + "_" + slotY + ".png");
+					ImageIO.write(img, "png", f);
+					
+				}catch(IOException e){
+					System.out.println("Error: " + e);
+				}
+				
 			}
-
-		}
-		
-		try{
-			f= new File("world.png");
-			ImageIO.write(img, "png", f);
-			
-		}catch(IOException e){
-			System.out.println("Error: " + e);
 		}
 		
 	}
